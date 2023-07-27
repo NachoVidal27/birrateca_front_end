bd05a5277d90ae2c93187407ec6de6370a92b319
simonabadcat <114427654+simonabadcat@users.noreply.github.com>
2023-07-27 17:24:59 -0300

    lo que quedo incompleto

--- src/routes/EditBeer.jsx
+++ src/routes/EditBeer.jsx
@@ -28,16 +28,16 @@ function EditBeer() {
     setBirra({ ...birra, style: e.target.value });
   };
   const handleAbv = (e) => {
-    setBirra({ ...birra, abv: e.target.abv });
+    setBirra({ ...birra, abv: e.target.value });
   };
   const handleLocation = (e) => {
-    setBirra({ ...birra, location: e.target.location });
+    setBirra({ ...birra, location: e.target.value });
   };
   const handleBrewDate = (e) => {
-    setBirra({ ...birra, brewDate: e.target.brewDate });
+    setBirra({ ...birra, brewDate: e.target.value });
   };
   const handleDescription = (e) => {
-    setBirra({ ...birra, description: e.target.description });
+    setBirra({ ...birra, description: e.target.value });
   };
 
   useEffect(() => {
@@ -65,53 +65,25 @@ function EditBeer() {
         <div className="md:mt-28">
           <form>
             <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
-              <label
-                htmlFor="style"
-                className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0"
-              >
+              <label htmlFor="style" className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0">
                 Estilo BJCP
               </label>
-              <input
-                className="border-2  mx-8 md:mx-2 roundeed"
-                type="text"
-                name="style"
-                defaultValue={birra.style}
-                onChange={handleStyle}
-              />
+              <input className="border-2  mx-8 md:mx-2 roundeed" type="text" name="style" defaultValue={birra.style} onChange={handleStyle} />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
-              <label
-                htmlFor="abv"
-                className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0"
-              >
+              <label htmlFor="abv" className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0">
                 Abv
               </label>
-              <input
-                className="border-2  mx-8 md:mx-2 roundeed"
-                type="text"
-                defaultValue={birra.abv}
-                onChange={handleAbv}
-              />
+              <input className="border-2  mx-8 md:mx-2 roundeed" type="text" defaultValue={birra.abv} onChange={handleAbv} />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
-              <label
-                htmlFor="location"
-                className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0"
-              >
+              <label htmlFor="location" className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0">
                 Ubicación
               </label>
-              <input
-                className="border-2 mx-8 md:mx-2 roundeed"
-                type="text"
-                defaultValue={birra.location}
-                onChange={handleLocation}
-              />
+              <input className="border-2 mx-8 md:mx-2 roundeed" type="text" defaultValue={birra.location} onChange={handleLocation} />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
-              <label
-                htmlFor="photo"
-                className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0"
-              >
+              <label htmlFor="photo" className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0">
                 Foto
               </label>
               <input className="border-2 mx-8 md:mx-2 roundeed" type="file" />
@@ -120,47 +92,27 @@ function EditBeer() {
               <div></div>
               <Link to="/tips">
                 <div className="w-[50%] md:w-[80%] mx-auto mt-2 border border-black rounded bg-black roundeed my-1 py-1 hover:scale-105">
-                  <h2 className="text-white text-sm hover:text-cream-dark  ">
-                    tips para fotografías
-                  </h2>
+                  <h2 className="text-white text-sm hover:text-cream-dark  ">tips para fotografías</h2>
                 </div>
               </Link>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
-              <label
-                htmlFor="brewDate"
-                className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0"
-              >
+              <label htmlFor="brewDate" className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0">
                 Fecha de elaboración
               </label>
-              <input
-                className="border-2 mx-8 md:mx-2 roundeed"
-                type="text"
-                defaultValue={birra.brewDate}
-                onChange={handleBrewDate}
-              />
+              <input className="border-2 mx-8 md:mx-2 roundeed" type="text" defaultValue={birra.brewDate} onChange={handleBrewDate} />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 mt-2">
-              <label
-                htmlFor="description"
-                className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0"
-              >
+              <label htmlFor="description" className="font-semibold md:font-normal font-roboto text-lg my-1 md:my-0">
                 Descripción
               </label>
-              <textarea
-                className="border-2 mx-8 md:mx-2 roundeed h-16 resize-none"
-                type="text"
-                defaultValue={birra.description}
-                onChange={handleDescription}
-              />
+              <textarea className="border-2 mx-8 md:mx-2 roundeed h-16 resize-none" type="text" defaultValue={birra.description} onChange={handleDescription} />
               <div></div>
             </div>
 
             <div></div>
             <div className="">
-              <button className="px-4 py-2 mt-6 bg-black text-white font-semibold rounded text-end">
-                Guardar
-              </button>
+              <button className="px-4 py-2 mt-6 bg-black text-white font-semibold rounded text-end">Guardar</button>
             </div>
           </form>
         </div>
